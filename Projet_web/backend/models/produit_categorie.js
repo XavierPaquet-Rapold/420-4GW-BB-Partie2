1337bfddad5d6b55adc8b7296739f13ec82fa358
Xavier Paquet-Rapold <1928107@bdeb.qc.ca>
2021-04-21 16:42:34 -0400

    Regler la connexion

--- Projet_web/backend/models/produit_categorie.js
+++ Projet_web/backend/models/produit_categorie.js
@@ -5,7 +5,12 @@ const produitCatSchema= new Schema({
 nom:{
     type:String,
     required:true
-},},
+},
+lien_image:{
+    type:String,
+    required:true
+}
+},
 {timestamps: true});
 const Magasin = mongoose.model('produit_categorie', produitCatSchema);
 module.exports = Magasin;
