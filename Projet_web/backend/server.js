bb2741a4ea671675f93725f9f9a1e213bb2131f0
Xavier Paquet-Rapold <1928107@bdeb.qc.ca>
2021-05-05 18:39:06 -0400

    Changements de derniere minute et verification qu'email unique

--- Projet_web/backend/server.js
+++ Projet_web/backend/server.js
@@ -1,19 +1,12 @@
 var express = require('express');
-var http = require('http');
 var app = express();
 var session = require('express-session');
 var cookieParser = require('cookie-parser');
-var path = require('path');
 var bodyParser = require('body-parser');
-var dateFormat = require('dateformat');
 const mongoose = require('mongoose');
-const { Console } = require('console');
-const JSONBig = require('json-bigint');
-var now = new Date();
 const siteTitle = "To Spite The Amish";
 const baseURL = "http://localhost:4000";
 const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 const { Client, Environment, ApiError } = require('square');
 // Set the Access Token which is used to authorize to a merchant
@@ -143,7 +136,15 @@ app.post('/process-payment', async (req, res) => {
 pour générer la page principale
 */
 app.get('/', function (req, res) {
-    
+    /**con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC", function (
+        err, result) {
+        res.render('pages/index', {
+            siteTitle: siteTitle,
+            pageTitle: "Page Principale",
+            items: result,
+            connexion: req.session.loggedin
+        });
+    });**/
     Promise.all([
         Produit_Categorie.find()
     ]).then(([categories]) => {
@@ -160,7 +161,16 @@ app.get('/', function (req, res) {
 pour generer la page de categorie
 */
 app.get('/categorie/:id', function (req, res) {
-
+    /**con.query("SELECT * FROM produit WHERE produit_catégorie_id_catégorie = (SELECT id_catégorie FROM produit_catégorie WHERE nom = ?); "+
+    "SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC", [req.params.id],
+        function (err, result) {
+            res.render('pages/categorie.ejs', {
+                siteTitle: siteTitle,
+                pageTitle: "Categorie",
+                outils: result[0],
+                items: result[1],
+                connexion: req.session.loggedin
+            });*/
     
     const query = { nom: req.params.id };
     Produit_Categorie.findOne(query)
@@ -185,7 +195,19 @@ app.get('/categorie/:id', function (req, res) {
 pour generer la page de produit
 */
 app.get('/produit/:id', function (req, res) {
- 
+   /**  con.query("SELECT id_produit, image, marque, nom, prix, description FROM produit WHERE nom = ?;" +
+    " SELECT b.nombre, c.nom, c.adresse, c.ville, c.code_postale, c.tel FROM produit a, inventaire b, magasin c WHERE a.id_produit = b.produit_id_produit AND a.nom = ? AND b.magasin_id_magasin = c.id_magasin;"+
+    " SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC;", [req.params.id, req.params.id],
+        function (err, result) {
+            res.render('pages/produit.ejs', {
+                siteTitle: siteTitle,
+                pageTitle: "Produit",
+                item: result[0],
+                outils: result[1],
+                items: result[2],
+                connexion: req.session.loggedin
+            });
+        });**/
     Produit.find({ nom: req.params.id })
     .then((produit) => {
         Promise.all([
@@ -211,7 +233,20 @@ app.get('/produit/:id', function (req, res) {
 pour generer la page de panier
 */
 app.get('/panier', function (req, res) {
- 
+    /**con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC;" + 
+    " SELECT produit.id_produit, panier.nombre, produit.image, produit.nom, produit.marque, produit.prix FROM panier, produit WHERE utilisateur_id_utilisateur = ? "+
+    "AND panier.produit_id_produit = produit.id_produit;", 
+    [req.session.id_utilisateur],
+
+    function (err, result) {
+        res.render('pages/panier.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Panier",
+            items: result[0],
+            outils: result[1],
+            connexion: req.session.loggedin
+        });
+    });**/
     Promise.all([
         Produit_Categorie.find(),
         Panier.aggregate([
@@ -242,7 +277,14 @@ app.get('/panier', function (req, res) {
 pour generer la page de connexion
 */
 app.get('/connexion', function (req, res) {
-  
+    /**con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC", function (
+        err, result) {
+        res.render('pages/connexion.ejs', {
+            siteTitle: siteTitle,
+            pageTitle: "Connexion",
+            items: result,
+            connexion: req.session.loggedin
+        });*/
         Promise.all([
             Produit_Categorie.find()
         ]).then(([result]) => {
@@ -259,7 +301,15 @@ app.get('/connexion', function (req, res) {
 pour generer la page de creation de compte
 */
 app.get('/creation', function (req, res) {
-  
+    /**con.query("SELECT * FROM produit_catégorie ORDER BY id_catégorie ASC",
+        function (err, result) {
+            res.render('pages/creation.ejs', {
+                siteTitle: siteTitle,
+                pageTitle: "Creation de compte",
+                items: result,
+                connexion: req.session.loggedin
+            });
+        });*/
         Promise.all([
             Produit_Categorie.find()
         ]).then(([result]) => {
@@ -316,7 +366,11 @@ Enlever un produit du panier
 app.post('/panier/enlever/:id', function (req, res) {
     var id = req.params.id;
     if(req.session.loggedin){
-        
+        /**con.query("DELETE FROM panier WHERE produit_id_produit = ? AND utilisateur_id_utilisateur = ?", [id_produit, req.session.id_utilisateur],
+        function (err, result) {
+            if (err) throw err;
+            res.redirect('/panier');
+        });**/
         Panier.findOneAndDelete(id, function (err) {
             if(err) console.log(err);
         });
@@ -334,7 +388,15 @@ Modifier la quantite d'un produit dans son panier
 */
 app.post('/panier/modifier/:id', function (req, res) {
     if(req.session.loggedin){
-  
+        /**con.query("UPDATE panier SET nombre = ? WHERE produit_id_produit = ? AND utilisateur_id_utilisateur = ?", [nombre, id_produit, req.session.id_utilisateur],
+        function (err, result) {
+            if (err) {
+                res.redirect(req.get('referer')); 
+            }
+            else{
+                res.status(204).send();
+            }
+        });**/
         Panier.findByIdAndUpdate(
             { _id: req.params.id },
             { nombre: req.body.quantity },
@@ -354,7 +416,24 @@ app.post('/panier/modifier/:id', function (req, res) {
 /**
  * Reception de connexion et mise en memoire
  */
-
+/**app.post('/connexion', function(req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    /**if (username && password) {
+        con.query('SELECT * FROM utilisateur WHERE email = ? AND mot_de_passe = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+                req.session.loggedin = true;
+                req.session.username = username;
+                req.session.id_utilisateur = results[0].id_utilisateur;
+                res.redirect('/panier');
+            } else {
+                res.status(204).send();
+            }
+        });
+    } else {
+        res.status(204).send();
+    }
+});*/
 app.post('/connexion', function(req, res){
     var username = req.body.username;
     var staySignedIn = req.body.signedIn;
@@ -383,21 +462,31 @@ app.post('/connexion', function(req, res){
  * post methode to date : pour ajouter un utilisateur a la BD
  */
 app.post('/creation', function (req,res){
-    var postale = req.body.Postale.split(" ").join("");
-    var tel = req.body.tel.split("-").join("");
-var userData ={
-    prenom : req.body.Prenom,
-    nom: req.body.Nom,
-    email: req.body.email,
-    password: req.body.password,
-    telephone: tel,
-    adresse: req.body.Adresse,
-    ville: req.body.ville ,
-    postale: postale
-}
-new Utilisateur(userData)
-.save()
-.then(userData=>{
-    res.redirect('/connexion')
-})
+    Promise.all([
+        Utilisateur.findOne({ email: req.body.email })
+    ]).then(([result]) => {
+        if(!result){
+            var postale = req.body.Postale.split(" ").join("");
+            var tel = req.body.tel.split("-").join("");
+        
+            var userData ={
+                prenom : req.body.Prenom,
+                nom: req.body.Nom,
+                email: req.body.email,
+                password: req.body.password,
+                telephone: tel,
+                adresse: req.body.Adresse,
+                ville: req.body.ville ,
+                postale: postale
+            }
+            new Utilisateur(userData)
+            .save()
+            .then(userData=>{
+                res.redirect('/connexion')
+            })
+        } else {
+            res.redirect('/creation')
+        }
+    })
+    
 });
